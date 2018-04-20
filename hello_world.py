ae55ba25c1a2ea9dc4e46c49c4b8eb522035fb0b
Illia Rahavoi <irahavoi@bbmtek.com>
2018-04-20 00:32:54 -0400

    Up

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import webbrowser
+
+
+name = "Illia"
+print("Hello World! \n My name is " + name + "and I'm starting my first steps with python.")
+webbrowser.open_new("http://python.org/")
